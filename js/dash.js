43ad95cbb9440cbd56c6b054a9499be9ecd30038
Marcos <marcosjoaomj76@gmail.com>
2026-04-22 18:56:35 -0300

    Feat: gauge js

---
+++ js/dash.js
@@ -0,0 +1,79 @@
+const ctxUmidade = document.getElementById('gaugeUmida');
+const ctx = document.getElementById('gaugeTemp');
+const maxUmidade = 100;
+const max = 40;
+
+async function lerDados () {
+  const res = await fetch('http://10.110.12.16:1880/sensor');
+  const dados = await res.json();
+
+  console.log(dados);
+
+  let valor = dados[0].temperatura; 
+  let valorUmidade = dados[0].umidade;
+  
+  new Chart(ctx, {
+  
+      type: 'doughnut',
+      data: {
+          datasets: [{
+              data: [valor, max - valor],
+              backgroundColor: ['#fb923c', '#e5e7eb'],
+              borderWidth: 0
+          }]
+      },
+      options: {
+          responsive: false,
+          maintainAspectRatio: false,
+          circumference: 180,
+          rotation: -90,
+          cutout: '70%',
+          plugins: {
+              legend: { display: false },
+              annotation: {
+                  annotations: {
+                      label: {
+                          type: 'doughnutLabel',
+                          content: [`${valor}°C`],
+                          font: [{ size: 20 }],
+                          color: ['#fb923c']
+                      }
+                  }
+              }
+          }
+      }
+      })
+  
+  new Chart(ctxUmidade, {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+        data: [valorUmidade, maxUmidade - valorUmidade],
+        backgroundColor: ['#60a5fa', '#e5e7eb'],
+        borderWidth: 0
+      }]
+    },
+    options: {
+      responsive: false,
+      maintainAspectRatio: false,
+      circumference: 180,
+      rotation: -90,
+      cutout: '70%',
+      plugins: {
+        legend: { display: false },
+        annotation: {
+          annotations: {
+            label: {
+              type: 'doughnutLabel',
+              content: [`${valorUmidade}%`],
+              font: [{ size: 20 }],
+              color: ['#60a5fa']
+            }
+          }
+        }
+      }
+    }
+  });
+};
+
+lerDados ();
